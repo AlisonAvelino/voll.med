c8d93b121a2f4793556124f25a0e1f9fcb5795e4
Alison Avelino <alisonavelino3133@gmail.com>
2025-09-29 16:11:50 -0300

    [TASK04] - feat: creating the controller for medical registration and dtos

---
+++ src/main/java/med/voll/api/medico/CadastroMedicoDTO.java
@@ -0,0 +1,7 @@
+package med.voll.api.medico;
+
+
+import med.voll.api.endereco.EnderecoDTO;
+
+public record CadastroMedicoDTO(String nome, String email, String crm, Especialidade especialidade, EnderecoDTO endereco) {
+}
